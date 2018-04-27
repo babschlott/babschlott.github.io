e4881dca248f986599694919532bf000fb1e7362
bikasm2 <bikasm2@gmail.com>
2018-04-27 09:15:21 -0400

    php

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+  // comentarios
+  $valor = 10+35;
+  echo "O resultado foi: " .$valor;
+  $texto="http://www.google.com";
+  echo "<a href='".$texto."'>Clique aqui</a>";
+?>
+  <script>
+    var valor=10+35;
+    alert(valor);
+  </script>
+
+</body>
+</html>
